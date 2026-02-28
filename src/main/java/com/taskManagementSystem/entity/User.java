7b986ecbb33bdc339ded58164f3687f28d55a06d
akshaycharjan <akshaycharjan007@gmail.com>
2026-02-28 22:59:08 +0530

    Pagination Json serialising issue Fixed

--- src/main/java/com/taskManagementSystem/entity/User.java
+++ src/main/java/com/taskManagementSystem/entity/User.java
@@ -1,5 +1,6 @@
 package com.taskManagementSystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Entity
 @Setter
 @Table(name = "users")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
     @Id
     @GeneratedValue
