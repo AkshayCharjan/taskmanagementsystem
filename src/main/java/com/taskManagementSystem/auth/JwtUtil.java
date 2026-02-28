c7407ed1b1ba9c74b90bec9d3ceeaaa44f76f74d
akshaycharjan <akshaycharjan007@gmail.com>
2026-02-28 13:48:33 +0530

    Added auth package and jwt dependencies

---
+++ src/main/java/com/taskManagementSystem/auth/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.taskManagementSystem.auth;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private SecretKey secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+}
